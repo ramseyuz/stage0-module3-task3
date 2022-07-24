64bf3ac36653490b33d959ec00e01919d988e2d7
Ramz <gulliyev95@gmail.com>
2022-07-24 12:04:54 +0500

    by Ramz

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -4,8 +4,11 @@ public class BasicVariablesInitialization {
 
 
     public static void main(String[] args) {
-        int first=1;
-        int second=10;
-        int third=100;
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
